1f89ed547b66095c969bff72355e58b09d12dcf8
iAkash <Akashsah946@gmail.com>
2022-10-20 08:53:03 +0545

    weatherinfo files added

--- public/main.js
+++ public/main.js
@@ -1,17 +1,13 @@
-const images = ["weather1.jpg","weather2.jpg","weather3.jpg","weather4.jpg","weather5.jpg"];
-var carousel = document.getElementById('carousel');
+var cityname = document.getElementById('cityname');
+function showalert(event){
+        if(event.key==="Enter"){
+           
+        } 
 
-const interval = ()=>{
-    setInterval(()=>{
-           setImages();
-    },3000);
-};
-interval();
-index = 0;
-const setImages = ()=>{
-    carousel.src = "./media/"+images[index];
-    index++;
-    if(index>images.length-1){
-        index=0;
-    }
+        
+        
+
+
+
+
 }
